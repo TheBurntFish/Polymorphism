adb46114952cad35150293172df94f0185030d00
Tian fishburn <tianfishburn@gmail.com>
2022-03-05 04:32:10 -0800

    Reflection

--- Polymorphism/Main.cpp
+++ Polymorphism/Main.cpp
@@ -23,3 +23,25 @@ int main() {
 	printArea(s1);
 	printArea(c1);
 }
+
+
+
+/*
+1. Which one of these do you think is easier 
+to implement and use, dynamic or static
+polymorphism?
+
+I very much think static polymorphism is easier to use and implement.
+Probably noticable based on the difference of time in my commits.
+
+
+
+2. Modern C++ development highly encourages 
+static polymorphism over dynamic
+polymorphism, why do you think that is?
+
+I think it is because templates are more compact and doesn't require you to add
+files to the parent file and this would probably take a long time with extremely
+large projects.
+
+*/
